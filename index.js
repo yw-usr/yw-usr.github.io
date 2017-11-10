82bfcf44a7c254ac81a1f8072bcccad573909326
Yui Wei <tyuiwei@gmail.com>
2017-11-11 02:27:21 +0800

    idek

--- index.js
+++ index.js
@@ -11,26 +11,32 @@
     function resetIntellectual() {
         $('.notintellectual').show();
         $('.intellectual2').hide();
+        $('#postlink').removeAttr("href");
     }
     function intellectualText() {
         $('.intellectual2').show();
         $('.notintellectual').hide();
+        $('#postlink').attr('href', "#body");
     }
     function resetResponsibility() {
         $('.notresponsibility').show();
         $('.responsibility2').hide();
+        $('#postlink').removeAttr("href");
     }
     function responsibilityText() {
         $('.notresponsibility').hide();
-        $('.responsibility2').show();
+        $('.responsibility2').show();        
+        $('#postlink').attr('href', "#body");
     }
     function resetComplex() {
         $('.notcomplex').show();
         $('.complex2').hide();
+        $('#postlink').removeAttr("href");
     }
     function complexText() {
         $('.notcomplex').hide();
-        $('.complex2').show();
+        $('.complex2').show();        
+        $('#postlink').attr('href', "#body");
     }
     $('#intellectual').hover(intellectualText, resetIntellectual);
     $('#responsibility').hover(responsibilityText, resetResponsibility);
